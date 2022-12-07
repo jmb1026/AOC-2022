6817ab518f7e6685e0ed957dc868c16039a273ba
Joe Burns <josebur86@gmail.com>
2022-12-07 07:34:07 -0600

    Day 04, Part 1

--- day04/Program.cs
+++ day04/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var pairs = File.ReadAllLines("input.txt");
+
+var count = 0;
+
+foreach(var input in pairs)
+{
+    var pair = input.Split(",");
+    var first = pair[0].Split("-");
+    var second = pair[1].Split("-");
+
+    var firstRange = new 
+    { 
+        Min = int.Parse(first[0]), 
+        Max = int.Parse(first[1]) 
+    };
+    var secondRange = new 
+    { 
+        Min = int.Parse(second[0]), 
+        Max = int.Parse(second[1]) 
+    };
+
+    if (secondRange.Min >= firstRange.Min && secondRange.Max <= firstRange.Max)
+    {
+        count++;
+    }
+    else if (firstRange.Min >= secondRange.Min && firstRange.Max <= secondRange.Max)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
