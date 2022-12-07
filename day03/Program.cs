c4e47ae4b62817bf1811a1f8e8aace410f6d7f08
Joe Burns <josebur86@gmail.com>
2022-12-06 23:15:51 -0600

    Day 03, Part 1

--- day03/Program.cs
+++ day03/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt");
+
+var totalPriority = 0;
+
+foreach(var rs in lines)
+{
+    var split = rs.Length / 2;
+
+    var firstItems = rs.Substring(0, rs.Length / 2);
+    var secondItems = rs.Substring(rs.Length / 2);
+
+    var item = firstItems.Intersect(secondItems).First();
+
+    var priority = (item - 64 <= 26) ? (26 + item - 64) : (item - 96);
+    totalPriority += priority;
+}
+
+Console.WriteLine(totalPriority);
