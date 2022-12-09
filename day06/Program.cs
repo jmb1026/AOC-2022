4f4a0ab7b3c083bcb64b4629c4a3a553ab5929f8
Joe Burns <josebur86@gmail.com>
2022-12-09 09:59:02 -0600

    Day 06, Part 1

--- day06/Program.cs
+++ day06/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var data = File.ReadAllText("input.txt");
+// Console.WriteLine(data);
+
+bool markerFound = false;
+for (var i = 3; i < data.Length && !markerFound; i++)
+{
+    var counts = new Dictionary<char, int>();
+    for (var j = 3; j >= 0; j--)
+    {
+        char c = data[i-j];
+        if (counts.ContainsKey(c))
+        {
+            break;
+        }
+
+        counts.Add(c, 1);
+    }
+
+    if (counts.Keys.Count == 4)
+    {
+        markerFound = true;
+        Console.WriteLine($"Character Process Count: {i+1}");
+    }
+}
