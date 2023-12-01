459f00324ed125844c12d14d1864874fc8053d95
Joe Burns <josebur86@gmail.com>
2023-12-01 16:34:23 -0600

    Update the starter files to take the input as a command line argument

--- 2023/server.js
+++ 2023/server.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 
+if (process.argv.length < 3) {
+    console.error("Not enough arguments");
+    return;
+}
+
+const file = process.argv[2];
+console.log("Input file: ", file);
+
 try {
-    const data = fs.readFileSync('input.txt', 'utf-8');
+    const data = fs.readFileSync(file, 'utf-8');
     const lines = data.split('\n');
 
     for (let i = 0; i < lines.length; ++i) {
