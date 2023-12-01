3c28c2f0ffde4ff94a49730e3094b82b595dd110
Joe Burns <josebur86@gmail.com>
2023-12-01 16:22:35 -0600

    01 Part 2

--- 2023/01/server.js
+++ 2023/01/server.js
@@ -6,13 +6,55 @@ try {
 
     let sum = 0;
     for (let i = 0; i < lines.length; ++i) {
-        const numbers = Array.from(lines[i]).filter((c) => c >= '0' && c <= '9');
-        if (numbers.length > 0) {
-            console.log(numbers[0], numbers[numbers.length - 1], parseInt(numbers[0] + numbers[numbers.length - 1]));
-            sum += parseInt(numbers[0] + numbers[numbers.length - 1]);
-        }
+        sum += extractNumbers(lines[i]);
     }
+
     console.log(sum);
 } catch (err) {
     console.error(err);
+}
+
+function extractNumbers(value) {
+    const characters = Array.from(value);
+    if (characters.length == 0) {
+        return 0;
+    }
+
+    const numbers = [];
+
+    for (let i = 0; i < characters.length; i++) {
+        const c = characters[i];
+        if (c >= '0' && c <= '9')
+        {
+            numbers.push({ pos: i, digit: c });
+        }
+    }
+
+    const words = [
+        { word: "one", digit: "1" },
+        { word: "two", digit: "2" },
+        { word: "three", digit: "3" },
+        { word: "four", digit: "4" },
+        { word: "five", digit: "5" },
+        { word: "six", digit: "6" },
+        { word: "seven", digit: "7" },
+        { word: "eight", digit: "8" },
+        { word: "nine", digit: "9" }
+    ];
+
+    for (let i = 0; i < words.length; i++) {
+        let index = value.indexOf(words[i].word);
+        while (index >= 0) {
+            numbers.push({ pos: index, digit: words[i].digit });
+
+            index = value.indexOf(words[i].word, index+1);
+        }
+    }
+
+    numbers.sort((a, b) => a.pos - b.pos);
+
+    const number = parseInt(numbers[0].digit + numbers[numbers.length-1].digit);
+    console.log(value, numbers[0].digit, numbers[numbers.length-1].digit, number);
+
+    return number;
 }
